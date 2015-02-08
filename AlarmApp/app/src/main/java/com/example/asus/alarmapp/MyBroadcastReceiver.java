387dd4336b03def9e8803883d85b7e5bd3bc6ef9
yollalala <yollandasekarrini@gmail.com>
2015-02-08 13:13:15 +0700

    Add Projet Tutorial 2

---
+++ AlarmApp/app/src/main/java/com/example/asus/alarmapp/MyBroadcastReceiver.java
@@ -0,0 +1,22 @@
+package com.example.asus.alarmapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+/**
+ * Created by asus on 2/8/2015.
+ */
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Waktu habis!!!!.",
+                Toast.LENGTH_LONG).show();
+        // Vibrate the mobile phone
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(2000);
+    }
+}
